7b5ea78ca18296b27ce784017ab94c6800fac6d3
LabibB <beshoy.labib@vodafone.com>
2022-09-29 22:26:08 +0200

    x

--- write_messages.py
+++ write_messages.py
@@ -14,8 +14,8 @@ students = [
 environment = Environment(loader=FileSystemLoader("templates/"))
 template = environment.get_template("sample_template.txt")
 
-# def opener(path, flags):
-#     return os.open(path, flags, 0o777)
+def opener(path, flags):
+    return os.open(path, flags, 0o777)
 
 for student in students:
     filename = f"message_{student['name'].lower()}.txt"
@@ -24,17 +24,8 @@ for student in students:
         max_score=max_score,
         test_name=test_name
     )
-    descriptor = os.open(
-    path='filename',
-    flags=(
-        os.O_WRONLY  # access mode: write only
-        | os.O_CREAT  # create if not exists
-        | os.O_TRUNC  # truncate the file to zero
-    ),
-    mode=0o777
-    )
-
+    #os.chmod(filename, 0o777)
     print(f"... not wrote {filename}")
-    with open(filename, mode="w", encoding="utf-8") as message:
+    with open(filename, mode="w", encoding="utf-8", opener=opener) as message:
         message.write(content)
         print(f"... wrote {filename}")
